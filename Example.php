1fe0e6ada07b4cae77ac6ba69e553feb8bce6846
Megharaj P N <meghrajdvg.pn253@gmail.com>
2024-04-23 00:19:38 +0530

    example pratice problems

--- Example.php
+++ Example.php
@@ -1,17 +1,22 @@
 <?php
 
-
-try {
-    // Code that may throw an exception
-    $result = 10/0; // This will cause a division by zero error-
-    echo "ssssssss".$result;
-} catch (Exception $e) {
-    // Catch the exception and handle it
-    echo "Caught exception: " . $e->getMessage();
-} finally {
-    // Code that always runs, regardless of whether an exception occurred
-    echo "\nFinally block executed.";
+$stars = '';
+for ($i = 1; $i <= 5; $i++) {
+    $stars .= '*'; // Add a star to the string
+    echo $stars . "\n"; // Output the string with stars and a newline
 }
+
+// try {
+//     // Code that may throw an exception
+//     $result = 10/0; // This will cause a division by zero error-
+//     echo "ssssssss".$result;
+// } catch (Exception $e) {
+//     // Catch the exception and handle it
+//     echo "Caught exception: " . $e->getMessage();
+// } finally {
+//     // Code that always runs, regardless of whether an exception occurred
+//     echo "\nFinally block executed.";
+// }
 
 
 ?>
